67eaea4fe4275a47efe997d0aa8049ee7acd1f7a
Marilud1 <cm.castilloc@uea.edu.ec>
2025-02-21 12:31:31 -0500

    Agregué un numero a cada ciudadano para facilitar el conteo

--- EstructuraDatos31012025/clasesPOO/semana10.cs
+++ EstructuraDatos31012025/clasesPOO/semana10.cs
@@ -1,48 +1,40 @@
 public static class Conjuntos{
     public static void run(){
-        Console.WriteLine(" semana 10- HashSet");
-Console.WriteLine();
-// El Gobierno Nacional a través de su Ministerio de Salud requiere algunos datos sobre la campaña de vacunación para la mitigación del COVID, en tal sentido se necesitan los siguientes datos:
-
-// Listado de ciudadanos que no se han vacunado
-// Listado de ciudadanos que han recibido las dos vacunas
-// Listado de ciudadanos que solamente han recibido la vacuna de pfizer
-// Listado de ciudadanos que solamente han recibido la vacuna de Astrazeneca 
-// Para resolver el ejercicio considere lo siguiente:
-
+      
         HashSet<string> ciudadanos = new HashSet<string>();
         HashSet<string> vacunados_pfizer = new HashSet<string>();
         HashSet<string> vacunados_astrazenica = new HashSet<string>();
-       
+
         // Leer ciudadanos registrados en archivos .txt
-        Console.WriteLine("CIUDADANOS REGISTRADOS");
+         Console.WriteLine();
+        Console.WriteLine("CIUDADANOS REGISTRADOS EN EL MINISTERIO DE SALUD");
         Console.WriteLine();
         LeerArchivo("ciudadanos.txt", ciudadanos);
         Console.WriteLine();
 
         Console.WriteLine("CIUDADANOS CON VACUNA PFIZER");
         Console.WriteLine();
-        LeerArchivo("vacunados pfizer.txt", vacunados_pfizer);
+        LeerArchivo("vacunados pfizer.txt", vacunados_pfizer);   
+        ImprimirListaNumerada(vacunados_pfizer);    
         Console.WriteLine();
 
         Console.WriteLine("CIUDADANOS CON VACUNA ASTRAZENECA");
         Console.WriteLine();
         
         LeerArchivo("vacunados astrazeneca.txt", vacunados_astrazenica);
+        ImprimirListaNumerada(vacunados_astrazenica);
         Console.WriteLine();
+        String registro;
 
         // Intersección de vacunados con ambas vacunas
-       
         Console.WriteLine("CIUDADANOS CON LAS DOS VACUNAS");
         Console.WriteLine();
         HashSet<string> dos_vacunas = Interseccion(vacunados_pfizer, vacunados_astrazenica);
 
-        if (dos_vacunas.Count > 1)
+        if (dos_vacunas.Count > 0)
         {
-            foreach (var ciudadano in dos_vacunas)
-            {
-                Console.WriteLine(ciudadano);
-            }
+            ImprimirListaNumerada(dos_vacunas);
+            Console.WriteLine("Ciudadanos con dos vacunas");
         }
         else
         {
@@ -51,57 +43,72 @@ Console.WriteLine();
 
         // Ciudadanos sin vacunas
         Console.WriteLine();
- 
         Console.WriteLine("CIUDADANOS SIN VACUNA");
-         Console.WriteLine();
+        Console.WriteLine();
         HashSet<string> sin_vacuna = new HashSet<string>(ciudadanos);
         sin_vacuna.ExceptWith(vacunados_pfizer);
         sin_vacuna.ExceptWith(vacunados_astrazenica);
 
-        // Mostrar ciudadanos sin vacuna en pantalla
-        if (sin_vacuna.Count > 1)
+        // Mostrar ciudadanos sin vacuna al momento de imprimir
+        if (sin_vacuna.Count > 0)
         {
-            Console.WriteLine("Lista de ciudadanos sin vacunas:");
             Console.WriteLine();
-            foreach (var ciudadano in sin_vacuna)
-            {
-                Console.WriteLine(ciudadano);
-            }
+            ImprimirListaNumerada(sin_vacuna);
         }
         else
         {
             Console.WriteLine("Todos los ciudadanos han sido vacunados");
         }
     
-        // metodo para leer los datos guardados
-        static void LeerArchivo(string archivo, HashSet<string> conjunto)
-         {
+
+    // Leer los registros
+     void LeerArchivo(string archivo, HashSet<string> conjunto)
+    {
         try
         {
-        StreamReader lector = new StreamReader(archivo);
+            StreamReader lector = new StreamReader(archivo);
+            string registro;
+            while ((registro = lector.ReadLine()) != null)
             {
-                string registro;
-                while ((registro = lector.ReadLine()) != null)
-                {
-                    conjunto.Add(registro);
-                    Console.WriteLine(registro);
-                }
+                conjunto.Add(registro);
+                Console.WriteLine(registro);
             }
+            lector.Close();
         }
         catch (Exception ex)
         {
             Console.WriteLine("Error al leer '" + archivo + "': " + ex.Message);
         }
-        }
+    }
 
-        // Función para calcular la intersección de dos conjuntos
-        HashSet<string> Interseccion(HashSet<string> conjunto1, HashSet<string> conjunto2)
+    // Imprimir los ciudadanos con su respectivo número
+    void ImprimirListaNumerada(HashSet<string> conjunto)
+    {
+        int contador = 1;
+        foreach (var item in conjunto)
         {
-            HashSet<string> interseccion = new HashSet<string>(conjunto1);
-            interseccion.IntersectWith(conjunto2);
-            return interseccion;
+            Console.WriteLine($"{contador}. {item}");
+            contador++;
         }
+    }
+
+    // Función para calcular la intersección de dos conjuntos
+     HashSet<string> Interseccion(HashSet<string> conjunto1, HashSet<string> conjunto2)
+    {
+        HashSet<string> interseccion = new HashSet<string>(conjunto1);
+        interseccion.IntersectWith(conjunto2);
+        return interseccion;
+    }
 
 
     }
 }
+
+
+
+
+
+
+
+    
+       
